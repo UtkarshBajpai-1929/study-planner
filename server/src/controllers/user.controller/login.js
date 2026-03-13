83b769e029910cc79c77c6f134f6d24aa20fd9b2
UtkarshBajpai-1929 <utkarshbajpai632@gmail.com>
2026-03-13 18:42:06 +0530

    cookie issue fixed

--- server/src/controllers/user.controller/login.js
+++ server/src/controllers/user.controller/login.js
@@ -24,7 +24,8 @@ const loginUser = asyncHandler(async(req,res)=>{
  const options =  {
   httpOnly: true,
   secure: true,
-  sameSite : "none"
+  sameSite : "none",
+  maxAge : 24 * 60 * 60 * 1000
  }
 
  return res.status(200)
--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,28 +1,37 @@
 import { apiError } from "../utils/apiError.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const verifyjwt =asyncHandler(async(req,res,next)=>{
-try {
-    const token = req.cookies?.accessToken;
-    if(!token){
-      throw new apiError(400, "Token not found");
+const verifyjwt = asyncHandler(async (req, res, next) => {
+  try {
+
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new apiError(401, "Token not found");
     }
-  
+
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    if(!decodedToken){
-      throw new apiError(400, "Unauthorised user");
+
+    const user = await User.findById(decodedToken._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new apiError(401, "Invalid token");
     }
-  
-    const user = await User.findById(decodedToken._id).select("-password -refreshToken");
-  
+
     req.user = user;
-  
+
     next();
-} catch (error) {
-  console.log(error);
-  throw new apiError(500, "Unauthorised request");
-}
+
+  } catch (error) {
+    console.log(error);
+    throw new apiError(401, "Unauthorised request");
+  }
 });
-export {verifyjwt};
+
+export { verifyjwt };
