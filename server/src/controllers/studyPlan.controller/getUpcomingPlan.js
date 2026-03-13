7eb65bb0e9be647ab00e1be385f5f33a6ca06db3
UtkarshBajpai-1929 <utkarshbajpai632@gmail.com>
2026-03-13 19:23:17 +0530

    git upcoming updated

--- server/src/controllers/studyPlan.controller/getUpcomingPlan.js
+++ server/src/controllers/studyPlan.controller/getUpcomingPlan.js
@@ -9,11 +9,13 @@ const getUpcomingTasks = asyncHandler(async (req, res) => {
     throw new apiError(401, "Unauthorized request");
   }
 
+  // Start of today
   const today = new Date();
-  today.setHours(0,0,0,0);
+  today.setHours(0, 0, 0, 0);
 
-  const nextDay = new Date();
-  nextDay.setDate(today.getDate() + 1);
+  // Start of tomorrow
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
 
   const studyPlan = await StudyPlan.findOne({
     user: req.user._id
@@ -23,16 +25,18 @@ const getUpcomingTasks = asyncHandler(async (req, res) => {
     throw new apiError(404, "Study plan not found");
   }
 
-  const upcoming = studyPlan.schedule.filter(day => {
+  // Get all days after today
+  const upcoming = studyPlan.schedule.filter((day) => {
     const d = new Date(day.date);
-    return d > today && d <= nextDay;
+    return d >= tomorrow;
   });
 
-  return res.status(200)
-  .json(new apiResponse(200, upcoming, "Upcoming tasks fetched"));
+  return res
+    .status(200)
+    .json(new apiResponse(200, upcoming, "Upcoming tasks fetched"));
 
 });
 
-export{
+export {
   getUpcomingTasks
-}
+};
